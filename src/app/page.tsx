c15855ff4cb12d46336c1a380520b88e7bbef41d
Huzaifa <huzaifaali417@gmail.com>
2024-12-08 16:16:05 +0500

    zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,45 @@
+import logo from "@/assets/logo.png";
+import resumePreview from "@/assets/resume-preview.jpg";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-      front page
-      <Link
-        href="/resumes"
-        className="ml-2 h-auto w-max rounded-lg bg-fuchsia-700 p-3 text-center text-white"
-      >
-        go to resumes
-      </Link>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-gray-100 px-5 py-12 text-center text-gray-900 md:flex-row md:text-start lg:gap-12">
+      <div className="max-w-prose space-y-3">
+        {/* .max-w-prose {
+    max-width: 65charaters;} */}
+        <Image
+          src={logo}
+          alt="Logo"
+          width={150}
+          height={150}
+          className="mx-auto md:ms-0"
+        />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Create the{" "}
+          <span className="inline-block bg-gradient-to-r from-green-600 to-green-400 bg-clip-text text-transparent">
+            Perfect Resume
+          </span>{" "}
+          in Minutes
+        </h1>
+        <p className="text-lg text-gray-500">
+          Our <span className="font-bold">AI resume builder</span> helps you
+          design a professional resume, even if you&apos;re not very smart.
+        </p>
+        <Button asChild size="lg" variant="premium">
+          <Link href="/resumes">Get started</Link>
+        </Button>
+      </div>
+      <div>
+        <Image
+          src={resumePreview}
+          alt="Resume preview"
+          width={600}
+          className="shadow-md lg:rotate-[1.5deg]"
+        />
+      </div>
     </main>
   );
 }
